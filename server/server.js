b1ab9177265a1b6fd0143914112d15a994901263
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-04-29 18:56:38 +0200

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -29,6 +29,15 @@ app.post('/todos',(req,res)=>{  // '/todos' for resource creation
   });
 }); //url and callback
 
+//GET to fetch data
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  }, (e)=>{
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
