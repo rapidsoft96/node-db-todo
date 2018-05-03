903058e55b947197179d2103025f0294b4f57f6f
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-05-03 13:48:31 +0200

    setup for heroku 2

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const {mongoose}=require('./../server/db/mongoose');
+const {ObjectID}=require('mongodb'); //import this to check if ID is valid.
+const {Todo}=require('./../server/models/todo');
+const {User}=require('./../server/models/user');
+
+//Todo.remove({}) - remove All
+Todo.remove({}).then((result)=>{
+  console.log(result);
+});
+
+// Todo.findOneAndRemove().then((result)=>{ //this method return object which is deleted
+//
+// });
+// Todo.findByIdAndRemove('5ae5d0f120d4d508b07f00df').then((todo)=>{
+//   console.log(todo);
+// });
