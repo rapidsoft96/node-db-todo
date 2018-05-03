1da39002b6f0682fea6a46a3a85bb2057df2a110
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-05-03 20:50:17 +0200

    Add DELETE /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,13 +4,13 @@ const {Todo}=require('./../server/models/todo');
 const {User}=require('./../server/models/user');
 
 //Todo.remove({}) - remove All
-Todo.remove({}).then((result)=>{
-  console.log(result);
-});
+// Todo.remove({}).then((result)=>{
+//   console.log(result);
+// });
 
 // Todo.findOneAndRemove().then((result)=>{ //this method return object which is deleted
 //
 // });
-// Todo.findByIdAndRemove('5ae5d0f120d4d508b07f00df').then((todo)=>{
-//   console.log(todo);
-// });
+Todo.findByIdAndRemove('5ae5d0f120d4d508b07f00df').then((todo)=>{
+  console.log(todo);
+});
