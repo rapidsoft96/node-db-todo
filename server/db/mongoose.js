fdbeb6d2f7add526b349f9e93b059a76c2f1b96b
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-05-03 13:54:18 +0200

    aaa

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://rapidsoft:vxwf0506@ds261929.mlab.com:61929/db-todo-api');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
