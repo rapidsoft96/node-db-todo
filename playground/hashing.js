c930282abd7d998d34f6d05331846885b2ce151d
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-05-07 11:29:09 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,27 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
-var data={
-  id:10
-};
-var token=jwt.sign(data,'123abc');  // object and secret ,this token we will send back to user when login
-console.log(token);
+const bcrypt=require('bcryptjs');
 
-var decoded=jwt.verify(token,'123abc');
-console.log(decoded);
+var password='123abc!';
+
+// bcrypt.genSalt(10,(err,salt)=>{
+//   bcrypt.hash(password,salt,(err,hash)=>{
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword='$2a$10$zw6ffsM0SkF0ELsTH2Yhze6HOxpbdfeNbt4mW1GSp97cvcpr0Nhoy';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+  console.log(res);
+});
+// var data={
+//   id:10
+// };
+// var token=jwt.sign(data,'123abc');  // object and secret ,this token we will send back to user when login
+// console.log(token);
+//
+// var decoded=jwt.verify(token,'123abc');
+// console.log(decoded);
 
 // var message='I am user number 3';
 //
