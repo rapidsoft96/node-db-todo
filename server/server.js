4a0b717edc1504ac7230ceaac4898a77953e011c
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-05-07 08:55:40 +0200

    add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
+var {authenticate}=require('./middleware/authenticate');
 
 var app=express();
 //port
@@ -119,6 +120,12 @@ app.post('/users',(req,res)=>{
 }).catch((e)=>{
     res.status(400).send(e);
 })
+});
+
+//MIDDLEWARE
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
 });
 
 
